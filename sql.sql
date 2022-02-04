92fe3d584b3f803eb51c762f324b3b7844c4af2b
Parrot Live user <user@localhost.localdomain>
2022-02-04 20:40:11 +0000

    script base de datos

--- sql.sql
+++ sql.sql
@@ -1,9 +1,5 @@
-CREATE DATABASE optimizedfinances;
-
-USE optimizedfinances;
-
 CREATE TABLE usuario(
-    id_usuario INT NOT NULL AUTO_INCREMENT,
+    id_usuario INT AUTO_INCREMENT NOT NULL,
     nombres VARCHAR(90) NOT NULL,
     correo VARCHAR(45) NOT NULL,
     clave VARCHAR(45) NOT NULL,
@@ -11,7 +7,7 @@ CREATE TABLE usuario(
 )ENGINE=InnoDB;
 
 CREATE TABLE acciones(
-    id_acciones INT NOT NULL AUTO_INCREMENT,
+    id_acciones INT AUTO_INCREMENT NOT NULL,
     tipo_acciones VARCHAR(45) NOT NULL,
     descripcion VARCHAR(200) NOT NULL,
     tipo_gasto VARCHAR(45) NOT NULL,
@@ -19,15 +15,17 @@ CREATE TABLE acciones(
     fecha_final DATE NOT NULL,
     valor BIGINT NOT NULL,
     id_usuario INT NOT NULL,
-    INDEX(id_usuario),
+    INDEX (id_usuario),
     PRIMARY KEY(id_acciones),
     CONSTRAINT fk_acciones
     FOREIGN KEY (id_usuario) 
-    REFERENCES usuario(id_usuario);
+    REFERENCES usuario(id_usuario)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 )ENGINE=InnoDB;
 
 CREATE TABLE salario(
-    id_salario INT NOT NULL,
+    id_salario INT AUTO_INCREMENT NOT NULL,
     valor BIGINT NOT NULL,
     periodo INT NOT NULL,
     id_usuario INT NOT NULL,
@@ -36,4 +34,6 @@ CREATE TABLE salario(
     CONSTRAINT fk_salario
     FOREIGN KEY (id_usuario)
     REFERENCES usuario(id_usuario)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 )ENGINE=InnoDB;
